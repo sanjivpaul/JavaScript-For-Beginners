3b73d905ecb4fd1453f485b1d9a6686035d4e2a6
Risav Singh <risavsingh7462@gmail.com>
2022-07-25 11:09:26 +0530

    02 DataTypes

---
+++ 02DataTypes.js
@@ -0,0 +1,55 @@
+// 02 DataTypes:
+/*There are two types of datatypes:
+A.Primitive
+    1.string
+    2.number
+    3.boolean
+
+B.Non-Primitive
+    4.object
+        null
+        function
+        array
+        jsObject
+
+5.undefined
+
+*/
+
+// --String DataTypes
+var studentName = "Sanjiv paul";//string
+console.log(studentName);
+
+// --Number datatype
+var mobileNumber = 8521542005; // number
+var marksStudent = 84.8; // float number
+console.log(mobileNumber);
+console.log(marksStudent);
+
+// --Boolean datatype
+var isPresent = true; //Boolean
+console.log(isPresent);
+
+// --Null datatype
+var studentSubject = null;
+console.log(studentSubject); //object
+
+// --Undefined datatype
+var parentMobile;
+console.log(parentMobile);
+
+// *******Type*********
+var type = typeof studentName;
+console.log(type);
+
+var type = typeof mobileNumber;
+console.log(type);
+
+var type = typeof isPresent;
+console.log(type);
+
+var type = typeof studentSubject;
+console.log(type);
+
+var type = typeof parentMobile;
+console.log(type);

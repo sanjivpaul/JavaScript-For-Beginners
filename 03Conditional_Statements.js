c0081803ed1cac39a80ef0bb0754df3c6bea4522
Risav Singh <risavsingh7462@gmail.com>
2022-07-25 11:17:25 +0530

    03 Conditional statement

---
+++ 03Conditional_Statements.js
@@ -0,0 +1,28 @@
+// 03 Conditional Statement:
+
+/*There are two types of conditional statements
+A.if else
+B.switch
+*/
+
+// A.if else:
+
+/*
+if (condition){
+    //code
+}else{
+    // code
+}
+*/
+
+
+var isVideoUploaded = false;
+
+if (isVideoUploaded == true) {
+  console.log("video is uploaded on the lms portal");
+} else {
+  console.log("sir please upload a video");
+}
+
+
+// B.switch
